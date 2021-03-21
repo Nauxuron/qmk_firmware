52399a3e74e47ab2799bd313fb2e2fe6cb9b6191
Wim de With <wf@dewith.io>
2021-03-21 15:49:12 +0100

    crkbd: wip

---
+++ keyboards/crkbd/keymaps/nauxuron/config.h
@@ -0,0 +1,51 @@
+/* Copyright 2021 Wim de With
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#define USE_SERIAL_PD2
+
+// Only require two taps to lock layer.
+#define TAPPING_TOGGLE 2
+
+// How long a key needs to be held to be registered as hold.
+#define TAPPING_TERM 200
+// If another key is pressed during the tapping term, register the tap as hold
+// instead.
+#define PERMISSIVE_HOLD
+// If a key is pressed but not released before a Mod-Tap key is released,
+// register it simply as a normal key press instead of a Mod + key press.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#ifdef RGBLIGHT_ENABLE
+    #undef RGBLED_NUM
+    #define RGBLIGHT_ANIMATIONS
+    #define RGBLED_NUM 27
+    #define RGBLIGHT_LIMIT_VAL 120
+    #define RGBLIGHT_HUE_STEP 10
+    #define RGBLIGHT_SAT_STEP 17
+    #define RGBLIGHT_VAL_STEP 17
+#endif
+
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
